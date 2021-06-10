383d021fd9aa86f690f695d1b6ba2f27ef2291e0
B.Fatih KOZ <fatih.koz@gmail.com>
2021-06-10 18:40:27 +0300

    SimBrief Booking State
    Update helpers to check SimBrief booking state too with user details.

--- helpers.php
+++ helpers.php
@@ -94,7 +94,7 @@ use Illuminate\Support\Facades\DB;
     // Format Aircraft State Badge
     // return formatted string (with html tags)
     if (!function_exists('Dispo_AcStateBadge')) {
-      function Dispo_AcStateBadge($state)
+      function Dispo_AcStateBadge($state, $aircraft_id = null)
       {
         $color = 'primary';
         if($state === 0) { $color = 'success'; }
@@ -102,6 +102,15 @@ use Illuminate\Support\Facades\DB;
         elseif($state === 2) { $color = 'warning'; }
 
         $result = "<span class='badge badge-".$color."'>".AircraftState::label($state)."</span>";
+
+        // See if this aircraft is being used by some user's active simbrief ofp
+        if (setting('simbrief.block_aircraft') && $aircraft_id && $state === 0) {
+          $simbrief_book = SimBrief::where('aircraft_id', $aircraft_id)->whereNotNull('flight_id')->whereNull('pirep_id')->orderby('created_at', 'desc')->first();
+          if (!empty($simbrief_book)) {
+            $result = "<span class='badge badge-secondary' title='Booked By: ".$simbrief_book->user->name_private."'>Booked</span>";
+          }
+        }
+
         return $result;
       }
     }
